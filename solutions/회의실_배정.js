09667690d4800e0583b60daaae76716c538f2a3a
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-06-25 13:29:28 +0900

    solved: 회의실 배정

---
+++ solutions/회의실_배정.js
@@ -0,0 +1,30 @@
+const INPUT_PATH = "./inputs/회의실_배정.txt";
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(inputArguments) {
+  const [N, ...list] = inputArguments;
+  const meetingTimes = [];
+  let currentTime = 0;
+  let meetingCount = 0;
+
+  for (let i = 1; i <= N; i++) {
+      const [startTime, endTime] = list[i].split(" ").map(Number);
+      meetingTimes.push({ startTime, endTime });
+  }
+
+  meetingTimes.sort((a, b) => a.endTime - b.endTime);
+
+  for (let i = 0; i < N; i++) {
+      if (currentTime === meetingTimes[i].startTime) {
+        currentTime = meetingTimes[i].endTime;
+        meetingCount++;
+      }
+  }
+
+  return meetingCount;
+}
+
+console.log(solution(input));
